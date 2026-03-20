36ab337e097bc1a7239af18b8670b5e1dd76a572
HeKtonk <hoyez.corey@gmail.com>
2026-03-20 17:34:50 +0100

    Update unittest notEq to test every property

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,7 +9,17 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
-    def test_notEq(self):
+    def test_notEqText(self):
+        node = TextNode("I'm a text node", TextType.LINK, "[link](https:boot.dev)")
+        node2 = TextNode("I'm another one text node", TextType.IMAGE, "![image](url/to/image.png)")
+        self.assertNotEqual(node, node2)
+
+    def test_notEqTextType(self):
+        node = TextNode("I'm a text node", TextType.LINK, "[link](https:boot.dev)")
+        node2 = TextNode("I'm another one text node", TextType.IMAGE, "![image](url/to/image.png)")
+        self.assertNotEqual(node, node2)
+
+    def test_notEqUrl(self):
         node = TextNode("I'm a text node", TextType.LINK, "[link](https:boot.dev)")
         node2 = TextNode("I'm another one text node", TextType.IMAGE, "![image](url/to/image.png)")
         self.assertNotEqual(node, node2)
