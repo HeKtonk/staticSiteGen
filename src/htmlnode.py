718eda9aac2be9ad64c3d15ca65ac5952d27ce3a
HeKtonk <hoyez.corey@gmail.com>
2026-03-23 19:22:02 +0100

    Add HTMLnode class with some unnittest

---
+++ src/htmlnode.py
@@ -0,0 +1,21 @@
+class HTMLNode():
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        props_to_html = ""
+        for prop in self.props:
+            props_to_html += f'{prop}="{self.props[prop]}" '
+        return props_to_html
+    
+    def __eq__(self, other):
+        return (isinstance(other, HTMLNode)) and (self.tag == other.tag) and (self.value == other.value) and (self.children == other.children) and (self.props == other.props)
+    
+    def __repr__(self):
+        return f"{self.tag}\n{self.value}\n{self.children}\n{self.props_to_html}"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHtmlNode(unittest.TestCase):
+    def test_eq(self):
+        node = HTMLNode()
+        node2 = HTMLNode()
+        self.assertEqual(node, node2)
+
+    def test_notEq(self):
+        node = HTMLNode("<a>")
+        node2 = HTMLNode()
+        self.assertNotEqual(node, node2)
+
+    def test_None_props(self):
+        node = HTMLNode()
+        self.assertIsNone(node.tag)
+        self.assertIsNone(node.value)
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
+
+    def test_to_html(self):
+        node = HTMLNode()
+        with self.assertRaises(NotImplementedError):node.to_html()
+
+    def test_props_to_html(self):
+        node = HTMLNode(None,None,None,{"target" : "_blank", "href" : "https://boot.dev"})
+        props_to_html = node.props_to_html()
+        self.assertEqual(props_to_html, 'target="_blank" href="https://boot.dev" ')
+
+if __name__ == "__main__":
+    unittest.main()
