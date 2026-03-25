b40d14db3532c90870a354a6fdb6ba3b4d96e554
HeKtonk <hoyez.corey@gmail.com>
2026-03-25 14:37:18 +0100

    Create ParentNode class

--- src/parentnode.py
+++ src/parentnode.py
@@ -1,13 +1,14 @@
 from htmlnode import HTMLNode
+import functools
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        super().__init__(tag, children, props=props)
+        super().__init__(tag, value=None, children=children, props=props)
 
     def to_html(self):
         if not self.tag:
-            raise ValueError("Missing tag")
+            raise ValueError(f"Missing tag : {self.tag}")
         if not self.children:
-            raise ValueError("Missing children")
+            raise ValueError(f"Missing children : {self.children}")
         else:
-            return f'<{self.tag}>{self.children.to_html()}<{self.tag}/>'
+            return f'<{self.tag}>{" ".join(str(child.to_html()) for child in self.children)}</{self.tag}>'
