e89602b04426e9079bf9505b63dd343cb9f79336
HeKtonk <hoyez.corey@gmail.com>
2026-03-25 17:55:49 +0100

    Add TextType.TEXT test for the text_node_to_html_node method

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -29,12 +29,18 @@ class TestTextNode(unittest.TestCase):
         self.assertIsNone(node.url)
 
     def test_urlNotNone(self):
-        node = TextNode("I'm a text node", TextType.PLAIN, "Plain text")
+        node = TextNode("I'm a text node", TextType.TEXT, "Plain text")
         self.assertIsNotNone(node.url)
 
     def test_falseProperty(self):
         node = TextNode("I'm a strange text node", "Very strange")
         self.assertNotIsInstance(node.text_type, TextType)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = TextNode.text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
                                  
 if __name__ == "__main__":
     unittest.main()
